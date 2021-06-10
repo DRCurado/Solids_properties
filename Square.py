13898db5c7c964143869c8e1e89d85f3dd3c2a95
DRCurado <55668064+DRCurado@users.noreply.github.com>
2021-06-10 00:25:44 -0300

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,34 @@
+#sqare shape
+#Calculate the CG of a square.
+
+import math
+
+#insert the inuts of your shape:
+unit = "mm"
+
+#insert the dimensions of your shape:
+Height = 10;
+Length = 200;
+
+#Software do the calculations
+Area = Height*Length;
+
+Cgx = Height/2;
+Cgy = Length/2;
+
+Ix= (Length*Height**3)/12;
+Iy= (Height*Length**3)/12;
+
+#Software print the results:
+print("");
+print(" ");
+print("Welcome to the Square Properties Calculator");
+print("-----------------");
+print("Area: "+str(Area)+unit+"^2");
+print("-----------------");
+print("CGx: "+str(Cgx)+unit);
+print("CGy: "+str(Cgy)+unit);
+print("-----------------");
+print("Moment of inertia:");
+print("X axix: "+str(Ix)+unit+"^4");
+print("Y axix: "+str(Iy)+unit+"^4");
